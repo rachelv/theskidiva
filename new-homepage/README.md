a0136155f51bf69be17dd021ba65e728f9eb4fbc
Rachel Vecchitto <rachel@magnetbox.org>
2023-08-09 12:53:41 -0600

    new homepage WIP

---
+++ new-homepage/README.md
@@ -0,0 +1,4 @@
+Build main CSS:
+```
+npx tailwindcss -i ./homepage-src.css -o ./homepage-dist.css --watch
+```
---
+++ new-homepage/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'sd-purple': {
+          DEFAULT: '#1e73be',
+        },
+        'sd-blue': {
+          DEFAULT: '#165389',
+        },
+        'sd-pink': {
+          DEFAULT: '#ee4d73',
+          hover: '#f92d5e',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
